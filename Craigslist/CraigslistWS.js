7c4870bc412da99cc8d5ee333bae60269acecec1
Derek Shibata <dkshibat@ucsd.edu>
2019-05-05 17:15:05 -0700

    Added prelim map scrape

--- Craigslist/CraigslistWS.js
+++ Craigslist/CraigslistWS.js
@@ -2,14 +2,17 @@
 
 const rp = require('request-promise');
 const $ = require('cheerio');
-const url = 'https://sandiego.craigslist.org/ssd/apa/d/imperial-beach-amazing-townhouse/6873411174.html';
 
-rp(url)
-    .then(function(html) {
-        console.log('Address: ' + $('div[class=mapaddress]', html).text());
-        console.log('Size: ' + $('.housing', html).text());
-        console.log('Price: ' + $('.price', html).text());
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
+const CLParse = function(url) {
+    rp(url)
+        .then(function(html) {
+                console.log('Address: ' + $('div[class=mapaddress]', html).text());
+                console.log('Size: ' + $('.housing', html).text());
+                console.log('Price: ' + $('.price', html).text());
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+};
+
+module.exports = CLParse;
