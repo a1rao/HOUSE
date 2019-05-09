0ded87c09001de43296009dca5c5425aa66b5787
Derek Shibata <dkshibat@ucsd.edu>
2019-05-08 17:24:50 -0700

    Edited mapscraper

--- MapScraping/MapScrape.js
+++ MapScraping/MapScrape.js
@@ -1,27 +1,26 @@
 const googleMapsClient = require('@google/maps').createClient({
-    key : 'AlzaSyBOItxSbHWVs44HvGno_ALoVcDEFMVOSPQ'
+    key : 'AIzaSyBOltxSbHWVs44HvGno_ALoVcDEFMVOSPQ'
 });
 
 function getDistance(address, campus) {
-    var distance = new googleMapsClient..DistanceMatrixService();
-    distance.getDistanceMatrix({
+    var distance = new googleMapsClient.distanceMatrix(
+    {
         origins: address,
-        destination: campus,
-        travelMode: google.maps.TravelMode.DRIVING,
-        unitSystem: google.maps.UnitSystem.METRIC,
-        durationInTraffic: true,
-        avoidHighways: false,
-        avoidTolls: true,
+        destinations: campus,
+        mode: 'driving',
+        units: 'metric',
+        avoid: 'tolls',
     },
-    function (response, status) {
-        if (status !== google.maps.DistanceMatrixStatus.OK) {
-            console.log('Error:', status);
-        } else {
+    function (err, response) {
+        if (!err) {
             console.log(response);
             $("#distance").text(response.rows[0].elements[0].distance.text).show();
             $("#duration").text(response.rows[0].elements[0].duration.text).show();
+        } else {
+            console.log(err);
         }
     });
 }
-
-getDistance('Regents La Jolla','9500 Gilman Dr, La Jolla, CA 92093');
+var start = ['Regents La Jolla'];
+var end = ['9500 Gilman Dr, La Jolla, CA 92093'];
+getDistance(start, end);
