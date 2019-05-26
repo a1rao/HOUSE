d730c3f2257c004fb0e400755a5de351e61ebbfb
NiyonikaS <njsheth@ucsd.edu>
2019-05-26 15:58:17 -0700

    save urls to database

--- react-firebase/src/components/Listings/addURL/AddNewURL.js
+++ react-firebase/src/components/Listings/addURL/AddNewURL.js
@@ -1,7 +1,32 @@
 import React, {Component} from "react";
+import app from "../../../base";
 
 // To Do
 function SaveListing(){
+    // Get user input
+    var userInput = document.getElementById("insertURL");
+    var url = userInput.elements[0].value;
+    var selectedFolder = "TEST";
+
+    // Add listing to "listings" table
+    var databaseref = app.database().ref('listings/');
+    console.log(url);
+    var listingKey = databaseref.push().key;
+    databaseref.child(listingKey).set({
+        'name': 'yay im not homeless anymore',
+        'address' : 'offcampus',
+        'price': 2700
+    });
+
+    // Add listing to folder specified by folder
+    var uid = app.auth().currentUser.uid;
+    databaseref = app.database().ref('users/' + uid + '/folders/' + selectedFolder);
+    databaseref.set({
+        'listing' : listingKey
+    });
+
+    // Reset fields
+    userInput.reset();
 
 }
 
@@ -10,7 +35,7 @@ class AddNewURL extends Component {
     render() {
         return(
             <div>
-                <form id="insertURL">
+                <form id = "insertURL">
                     <input
                         name="url"
                         type="text"
