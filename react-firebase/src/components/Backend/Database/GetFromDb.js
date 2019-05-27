e575f122413e085f8438376b6312234db0ada4f3
NiyonikaS <njsheth@ucsd.edu>
2019-05-27 14:35:55 -0700

    added backend folders

---
+++ react-firebase/src/components/Backend/Database/GetFromDb.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import app from "../../../base";
+
+/*
+ * Functions that will perform queries to get information from the database
+ */
+
+
+// Get IDs of saved listings in
+function getIDs(){
+
+}
