dd407b3a95caac8af085f63efcc37265042fa748
jhrose <jhrose@ucsd.edu>
2019-06-02 23:46:41 -0700

    Shit kind of works (I hope).

--- react-firebase/src/components/Comparison Page/List Table 1/Table1.js
+++ react-firebase/src/components/Comparison Page/List Table 1/Table1.js
@@ -33,7 +33,6 @@ class Table1 extends Component {
     }
 
     handleShow() {
-<<<<<<< HEAD
         this.setState({ show: true });
 
     }
@@ -44,9 +43,8 @@ class Table1 extends Component {
             <Button variant="outline-success" onClick={() => this.handlePush(eachFolder)}>{eachFolder}</Button>
         );
         allFolders = all;
-=======
+
         this.setState({ showMain: true });
->>>>>>> a0fa4313e8710401c1b506413f26f7a32d1b30af
     }
 
 
@@ -57,7 +55,6 @@ class Table1 extends Component {
                 <Button variant="primary" onClick={this.handleChooseFolders}> Add New Listing
                 </Button>
 
-<<<<<<< HEAD
                 {/*<Modal show={this.state.show} onHide={this.handleClose}>*/}
                 {/*    <Modal.Header closeButton>*/}
                 {/*        <Modal.Title>Table {this.state.tableNumber}</Modal.Title>*/}
@@ -65,9 +62,7 @@ class Table1 extends Component {
                 {/*</Modal>*/}
 
                 <Modal show={this.state.showFolders} onHide={this.handleCloseFolders}>
-=======
-                <Modal show={this.state.showMain} onHide={this.handleClose}>
->>>>>>> a0fa4313e8710401c1b506413f26f7a32d1b30af
+
                     <Modal.Header closeButton>
                         <Modal.Title>Select a Folder</Modal.Title>
                     </Modal.Header>
